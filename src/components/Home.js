df72a7e0c5277a7330e98d0d608a8695274d2370
jayaprakash1207 <jpkash2001@gmail.com>
2022-10-09 19:22:08 +0530

    Blog Demo Completed

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Bloglist from "./Bloglist"
+import useFetch from "./useFetch";
+
+const Home = () => {
+    const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
+
+    return ( 
+        <div className="home">
+            {error && <div> {error} </div>}
+            {isLoading && <div>loading..</div>}
+            {blogs && <Bloglist blogs = { blogs } title = "All Blogs!" />}
+        </div>
+     );
+}
+ 
+export default Home;
